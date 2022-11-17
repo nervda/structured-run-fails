780ffd3b38a0296f32e504719a57cd7b7beb7527
William Lee <38011201+nervda@users.noreply.github.com>
2022-11-17 14:52:26 -0500

    Create parent.tf

---
+++ parent.tf
@@ -0,0 +1,14 @@
+terraform{
+required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.39.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+module "foo"{
+    source = "./module"
+}
